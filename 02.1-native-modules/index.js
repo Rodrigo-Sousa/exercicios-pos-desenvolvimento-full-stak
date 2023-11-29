0e4eab7c8a3025ebccfce8431e57ed7b10d5ea8e
Rodrigo Sousa - Personal <rodrigo.sdeveloper@gmail.com>
2023-11-29 08:12:55 -0300

    Adicionado o conteúdo utilizando a promises - fazendo uso do promise hell

--- 02.1-native-modules/index.js
+++ 02.1-native-modules/index.js
@@ -1,10 +1,41 @@
+// Realizando a utilização das promises, com o alias fs
+
+import {promises as fs} from "fs";
+
+
+// Utilizando a promises. Utilizando o arrow function
+
+fs.writeFile("teste-arquivo-promises.txt", "conteúdo realizado utilizando a promises").then( () => {
+
+    // Então, caindo aqui, iremos utilizar o appendFile();
+    fs.appendFile("teste-arquivo-promises.txt","\n Testando a concatenação dos arquivos, com promises \n").then(()=>{
+        // Lendo o arquivo
+        fs.readFile("teste-arquivo-promises.txt", "utf-8").then((response)=>{
+            // pegando o response da promises
+            console.log(response);
+        }).catch((erro) => {
+            // Mensagem de erros.
+            console.log(erro);
+        });
+
+    }).catch(()=>{
+        console.log("Erro, ao tentar rodar o appendFile");
+    });
+    
+}).catch(() => {
+    // Caso dê algum erro.
+    console.log(erro);
+});
+
+
 // Para realizarmos a importação, inserir a palavra import, o nome da variável seguida de from entre aspas, o nome do módulo que iremos importar
 
 // fs é o módulo nativo do Node.Js, sendo ele o file system
-import fs from "fs";
+// import fs from "fs";
 
 // Escrevendo em um arquivo. Se o arquivo que passamos, não existir, esse método criará ele para nós. Como parâmetros, passamos o primeiro, que é o nome do arquivo, e o segundo o conteúdo do arquivo. O terceiro parâmetro é uma callback, ela, no caso do WriteFile, tera um único parâmetro, que é o responsável por erro, caso ocorra.
 
+/* Utilizando callbacks */
 // fs.writeFile("teste.txt", "teste, teste, teste, conteúdo do arquivo.", function(err){
 //     // Após rodar essa função, ele irá retornar o callback, após escrever o conteúdo dentro do arquivo que passamos.
 //     if(err){
@@ -34,14 +65,14 @@ import fs from "fs";
 /* Utilizando de forma sincrona*/
 // Utilizando o writeFileSync, não precisamos fazer a utilização de callbacks. Utilizando o async await.
 
-try{
-    fs.writeFileSync("teste-write-file-sync.txt", "conteúdo do arquivo, utilizando o file sync");
+// try{
+//     fs.writeFileSync("teste-write-file-sync.txt", "conteúdo do arquivo, utilizando o file sync");
 
-    const data = fs.readFileSync("teste-write-file-sync.txt", "utf-8");
+//     const data = fs.readFileSync("teste-write-file-sync.txt", "utf-8");
 
-    console.log(data);
-} catch{
-    console.log(err);
-}
+//     console.log(data);
+// } catch{
+//     console.log(err);
+// }
 
 // Devemos evitar trabalhar desta forma em API's, para não travarmos a execução, enquanto não obtem a resposta do que foi solicitado.
